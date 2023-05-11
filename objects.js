c236ee15254ead8f49148bb97a57be7b6f927749
Hailyee <130278234+Hailyee@users.noreply.github.com>
2023-05-11 11:16:09 +0900

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,18 @@
+/*객체(object)
+*
+* - 속성의 모음으로 이루어진 데이터
+* - 속성에는 어떤 데이터 타입이라도 할당 가능
+* - 속성의 구분 , 
+* - 속성의 정의 속성명:속성값
+*/
+
+
+var student = {
+    grade: 1,
+    school: "lion school"
+};
+
+
+console.log(student);
+console.log(typeof(student));
+
